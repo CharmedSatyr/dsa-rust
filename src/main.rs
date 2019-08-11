cf7d77a3d541adae861c4daeab95810a8fbb4c72
Joseph <z@charmed.tech>
2019-08-10 21:08:09 -0700

    Nest challenges in algorithms folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // import the modules that need to be tested
 // allow dead code since main calls no fns
-pub mod fibonacci;
+pub mod algorithms;
 
 fn main() {
     println!("Run `cargo test` to see these modules in action!");
